13bb59808e681fe2e033b4a26fd823066a06732e
suhejian <hejiansu@126.com>
2022-05-12 10:18:58 +0800

    update get_prediction function

--- utils.py
+++ utils.py
@@ -109,3 +109,52 @@ def cal_f1(c, p, r):
     if r and p:
         return 2 * p * r / (p + r), p, r
     return 0, p, r
+
+
+
+def get_predictions(outputs, entities, length):
+    predictions = []
+    for index, (instance, ent_set, l) in enumerate(zip(outputs, entities, length)):
+        forward_dict = {}
+        head_dict = {}
+        ht_type_dict = {}
+        for i in range(l):
+            for j in range(i + 1, l):
+                if instance[i, j] == 1:
+                    if i not in forward_dict:
+                        forward_dict[i] = [j]
+                    else:
+                        forward_dict[i].append(j)
+        for i in range(l):
+            for j in range(i, l):
+                if instance[j, i] > 1:
+                    ht_type_dict[(i, j)] = instance[j, i]
+                    if i not in head_dict:
+                        head_dict[i] = {j}
+                    else:
+                        head_dict[i].add(j)
+
+        predicts = []
+
+        def find_entity(key, entity, tails):
+            entity.append(key)
+            if key not in forward_dict:
+                if key in tails:
+                    predicts.append(entity.copy())
+                entity.pop()
+                return
+            else:
+                if key in tails:
+                    predicts.append(entity.copy())
+            for k in forward_dict[key]:
+                find_entity(k, entity, tails)
+            entity.pop()
+
+        for head in head_dict:
+            find_entity(head, [], head_dict[head])
+
+        predicts = set([convert_index_to_text(x, ht_type_dict[(x[0], x[-1])]) for x in predicts])
+        
+        predictions.append(predicts)
+    
+    return predictions
